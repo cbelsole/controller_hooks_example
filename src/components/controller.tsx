2f14444f17a796308047cad585d36d364cd0de26
Chris Belsole <cbelsole@gmail.com>
2020-03-14 10:45:54 -0400

    cleanup unused components

--- src/components/controller.tsx
+++ src/components/controller.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { getTodos, completeTodo as completeTodoAPI, iTodo } from '../api/todos';
 import TodoList from './todoList';
 
-interface Props {}
-
 const Controller: React.FunctionComponent = () => {
   const [todos, setTodos] = useState<iTodo[]>([]);
 
